26366b95d6b6ce0aea82705b9c7db6b0f877b1e5
alwsecond <150939493+alwsecond@users.noreply.github.com>
2024-07-06 15:02:50 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,3 @@
-# Decompiled with PyLingual (https://pylingual.io)
-# Internal filename: server.py
-# Bytecode version: 3.11a7e (3495)
-# Source timestamp: 1970-01-01 00:00:00 UTC (0)
-
 from bottle import route, run, template, request, response
 import base64
 import socket
